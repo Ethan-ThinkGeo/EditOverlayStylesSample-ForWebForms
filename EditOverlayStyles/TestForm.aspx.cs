02fcf5d72e0a908db78a184961daf213d7d9023b
Mike-ThinkGeo <mikeyu@thinkgeo.com>
2016-12-08 17:42:49 +0800

    Upload code.

---
+++ EditOverlayStyles/TestForm.aspx.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Configuration;
+using ThinkGeo.MapSuite.Core;
+using ThinkGeo.MapSuite.WebEdition;
+
+namespace EditOverlayStyles
+{
+    public partial class TestForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                Map1.MapBackground.BackgroundBrush = new GeoSolidBrush(GeoColor.FromHtml("#E5E3DF"));
+                Map1.CurrentExtent = new RectangleShape(-10779959,3909739,-10777699,3908399);
+                Map1.MapUnit = GeographyUnit.Meter;
+
+                Map1.MapTools.OverlaySwitcher.Enabled = true;
+                Map1.MapTools.MouseCoordinate.Enabled = true;
+
+                //Adds the Google Map as an overlay
+                GoogleOverlay google = new GoogleOverlay("Google Map");
+                google.JavaScriptLibraryUri = new Uri(ConfigurationManager.AppSettings["GoogleUri"]);
+                google.GoogleMapType = GoogleMapType.Normal;
+                Map1.CustomOverlays.Add(google);
+
+                //Creates polygon feature to be added to the EditOverlay
+                PolygonShape polygonShape = new PolygonShape();
+                RingShape ringShape = new RingShape();
+                ringShape.Vertices.Add(new Vertex(-10778968, 3909448));
+                ringShape.Vertices.Add(new Vertex(-10778686, 3909443));
+                ringShape.Vertices.Add(new Vertex(-10778691, 3909180));
+                ringShape.Vertices.Add(new Vertex(-10778982, 3909175));
+                ringShape.Vertices.Add(new Vertex(-10778968, 3909448));
+                polygonShape.OuterRing = ringShape;
+
+                Feature editFeature = new Feature(polygonShape);
+                Map1.EditOverlay.Features.Add(editFeature);
+
+                //Sets the properties so that the features can be only draggable.
+                //Notice that we don't set the style here. We set the style in javascript in TestForm.aspx under the script tag.
+                Map1.EditOverlay.TrackMode = TrackMode.Edit;
+                Map1.EditOverlay.EditSettings.IsDraggable = true;
+                Map1.EditOverlay.EditSettings.IsReshapable = false;
+                Map1.EditOverlay.EditSettings.IsResizable = false;
+                Map1.EditOverlay.EditSettings.IsRotatable = false;
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,41 @@
+# EditOverlayStylesSample-ForWebForms
+
+### Description
+
+In today’s Web project, we learn how to control the styles of the EditOverlay for both the default style and the editing style. In order to accomplish this, we write some javascript code. Look at the code in the “script” tag to see how we can control the fill color, the opacity, the border width etc of the features in the EditOverlay.
+
+![Screenshot](https://github.com/ThinkGeo/EditOverlayStylesSample-ForWebForms/blob/master/ScreenShot.png)
+
+### Requirements
+
+This sample makes use of the following NuGet Packages
+
+[MapSuite 10.0.0.0](http:mapsuite.nuget)
+
+### About the Code
+
+Working...
+
+### Getting Help
+
+[Map Suite WebForms For Winforms Wiki Resources](http://wiki.thinkgeo.com/wiki/map_suite_webforms_edition)
+
+[Map Suite WebForms For Winforms Product Description](http://thinkgeo.com/map-suite-developer-gis/webforms-edition/)
+
+[ThinkGeo Community Site](http://community.thinkgeo.com/)
+
+[ThinkGeo Web Site](http://www.thinkgeo.com)
+
+### Key APIs
+
+This example makes use of the following APIs:
+
+Working...
+
+### About Map Suite
+
+Map Suite is a set of powerful development components and services for the .Net Framework.
+
+### About ThinkGeo
+
+ThinkGeo is a GIS (Geographic Information Systems) company founded in 2004 and located in Frisco, TX. Our clients are in more than 40 industries including agriculture, energy, transportation, government, engineering, software development, and defense.
